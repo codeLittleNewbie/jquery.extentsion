fc9d8f9852cfd8efb7076f9fa5ea093cd1fdf571
李炜强 <W-Q@weiqiangdeMacBook-Pro.local>
2017-06-29 09:18:32 +0800

    修改了extension框架

---
+++ jquery.extension.js
@@ -0,0 +1,37 @@
+/**
+ * Created by W-Q on 2017/6/29.
+ */
+/*!
+ * jQuery Color Animations v@VERSION
+ * https://github.com/jquery/jquery-color
+ *
+ * Copyright jQuery Foundation and other contributors
+ * Released under the Apache2.0 license.
+ * http://jquery.org/license
+ *
+ * Date: @DATE
+ */
+(function (jQuery, undefined) {
+    // 1.给jquery添加一个静态方法,作为构造函数
+    extension = jQuery.extension = function (param) {
+        return new jQuery.extension.fn.parse(param);
+    };
+
+    // 2.设置extension的原型对象
+    extension.fn = extension.prototype = {
+        parse : function (param) {
+            console.log(param);
+        }
+    };
+
+    // 3.修改原型
+    jQuery.extension.fn.parse.prototype = extension.fn;
+
+    // 4.接口函数
+    extension.extend = extension.fn.extend = function (obj) {
+        for(var attr in obj){
+            this[attr] = obj[attr];
+        }
+    }
+
+})(jQuery);
